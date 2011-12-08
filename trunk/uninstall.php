ca604a08980ad878eae552866a0a88046b0d664d
blazersix <blazersix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-08 22:57:28 +0000

    Adding version 0.2
    git-svn-id: https://plugins.svn.wordpress.org/simple-page-sidebars@472634 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/uninstall.php
+++ trunk/uninstall.php
@@ -6,4 +6,5 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 global $wpdb;
 
 $wpdb->query("DELETE FROM $wpdb->postmeta WHERE meta_key='_sidebar_name'");
+delete_option( 'simple_page_sidebars_default_sidebar' );
 ?>
